861ff1a54dad675da4b140a4706a3e848ad9764c
Gordon <gordonpeng27@gmail.com>
2025-05-08 19:24:34 +0800

    Added Merch Database

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Merchandise
 # Register your models here.
+
+class MerchandiseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    search_fields = ('name',)
+    list_filter = ('stock', 'price')
+
+admin.site.register(Merchandise, MerchandiseAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Merchandise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='merchandise/')
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
