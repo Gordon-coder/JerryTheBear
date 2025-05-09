4cfedb8e3d4bfefcae712ad1c99027be75084304
Gordon <gordonpeng27@gmail.com>
2025-05-09 09:50:05 +0800

    changed blank=True

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ class Merchandise(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='merchandise/', null=True)
+    image = models.ImageField(upload_to='merchandise/', null=True, blank=True)
     stock = models.IntegerField(default=0)
 
     def __str__(self):
