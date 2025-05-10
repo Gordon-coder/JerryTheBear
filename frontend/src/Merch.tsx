61bf1c8669efd2d498556ac6002183983fe41f4b
Gordon <gordonpeng27@gmail.com>
2025-05-10 22:13:17 +0800

    added form

--- frontend/src/Merch.tsx
+++ frontend/src/Merch.tsx
@@ -1,9 +1,14 @@
 import { useParams } from "react-router";
 import { useState, useEffect } from "react";
+import Navbar from './Navbar.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './App.css'
 
 function Merch() {
   const { id } = useParams<{ id: string }>()
   const [merch, setMerch] = useState({})
+  const [amount, setAmount] = useState(0)
 
   async function fetchMerch() {
       const response = await fetch('http://localhost:8000/api/merchandises/')
@@ -16,12 +21,35 @@ function Merch() {
     }, [])
 
   return (
-    
-    <div>
-      <h1>Merch</h1>
-      <p>Welcome to the Merch page! {id}</p>
-      <pre>{JSON.stringify(merch)}</pre>
-    </div>
+    <>
+      <Navbar />
+      <div className="showroom">
+        <img src={merch.image} alt={merch.name} className='merch-img'/>
+        <div className="merch-info">
+          <h1>{merch.name}</h1>
+          <hr />
+          <h4 className="price">${merch.price}</h4>
+        </div>
+      </div>
+      <form className="form">
+        <div className="form-floating mb-3">
+          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+          <label htmlFor="name" className="form-label">Name: </label>
+        </div>
+        <div className="form-floating mb-3">
+          <input type="text" className="form-control" id="phone" placeholder="Enter your phone number" />
+          <label htmlFor="phone" className="form-label">Phone number: </label>
+        </div>
+        <div className="form-floating mb-3">
+          <input type="number" className="form-control" id="amount" placeholder="Enter amount" onChange={(e) => setAmount(Number(e.target.value))} value="0"/>
+          <label htmlFor="amount" className="form-label">Amount: </label>
+        </div>
+        <div className="d-flex justify-content-between">
+          <div>Current Price: ${merch.price * amount}</div>
+          <input type="submit" className="btn btn-primary" value="Send" />
+        </div>
+      </form>
+    </>
   );
 }
 
--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
 function Navbar() {
-  const [active, setActive] = useState('home');
+  const [active, setActive] = useState('');
   const [rs, setRs] = useState(0);
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top">
-      <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top ">
+      <div className="container-fluid ">
         <a className="navbar-brand" onClick={() => setRs(rs + 1)}>{'Jerry the Bearrrrr'+'r'.repeat(rs)}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
